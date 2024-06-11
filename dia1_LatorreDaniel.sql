ad59c7a9a45f50c1be92d54361be048c122100de
Daniel Latorre <156432430+Latooo@users.noreply.github.com>
2024-06-11 08:12:23 -0500

    Add files via upload

---
+++ dia1_LatorreDaniel.sql
@@ -0,0 +1,27 @@
+-- #####################
+-- ### EJERCICIO # 1 ###
+-- #####################
+-- Creacion de la base de datos "tienda"
+CREATE DATABASE tienda;
+
+-- Usar base de datos "tienda"
+uSE tienda;
+
+-- Crear tabla fabricante
+create table fabricante(
+id int auto_increment primary key,
+nombre varchar(100)
+);
+
+-- Mostrar tablas
+show tables;
+
+-- Crear tabla "producto"
+create table producto(
+id int auto_increment primary key,
+nombre varchar(100),
+precio decimal(10,2),
+id_fabricante int,
+foreign key (id_fabricante) references fabricante(id)
+);
+-- Desarrollado por Daniel Latorre / ID.1.005.257.201
